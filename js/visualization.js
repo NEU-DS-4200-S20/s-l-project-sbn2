6b15577c05c44bf03671519ac9415eb26ae7f7b6
pparvani <pparvani@gmail.com>
2020-04-05 18:03:44 -0400

    adding files that may or may not be useful lol

--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,36 @@
 // Immediately Invoked Function Expression to limit access to our 
 // variables and prevent 
-((() => {
+var width = 960;
+var height = 500;
 
-  console.log("Hello, world!");
+var svg = d3
+  .select("#map-container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-})());
+var svg2 = d3
+  .select("#chart-container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+var projection = d3
+  .geoAlbersUsa()
+  .translate([width / 2, height / 2])
+  .scale(width);
+
+  var path = d3.geoPath().projection(projection);
+
+d3.json("us.json", function(us) {
+  //Error
+  d3.csv("data/Attendee Information - 2019.csv", function(Attendee) {
+  	drawMap(us, Attendee);
+  });
+});
+
+var brush = d3
+  .brush()
+  .on("start brush", highlight)
+  .on("end", brushend);
+
